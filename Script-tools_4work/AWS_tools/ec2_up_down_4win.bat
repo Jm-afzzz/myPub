777f6ae7e5ab2c1285b695f363d20a2178b14bcd
Jm-afzzz <159032455+Jm-afzzz@users.noreply.github.com>
2025-02-17 02:06:17 +0800

    Create ec2_up_down_4win.bat

---
+++ Script-tools_4work/AWS_tools/ec2_up_down_4win.bat
@@ -0,0 +1,21 @@
+::在Windows上設定工作排程器，用來定時開關AWS上的特定EC2機器
+
+::開機
+@echo off
+start cmd /k "echo EC2 machine status & ^
+aws ec2 describe-instances --filters ^"Name=instance-type,Values=ec2-type^" --query ^"Reservations[].Instances[].[Tags[].Value, InstanceId, PublicIpAddress, State.Name]^" --profile AWS-profile & ^
+timeout /t 10 & ^
+aws ec2 start-instances --instance-ids instance-id-1 instance-id-2 instance-id-3 instance-id-4 --profile AWS-profile & ^
+timeout /t 40"
+
+start cmd /k "echo EC2 machine new status && aws ec2 describe-instances --filters ^"Name=instance-type,Values=ec2-type^" --query ^"Reservations[].Instances[].[Tags[].Value, InstanceId, PublicIpAddress, State.Name]^" --profile AWS-profile" 
+
+::關機
+@echo off
+start cmd /k "echo EC2 machine status & ^
+aws ec2 describe-instances --filters ^"Name=instance-type,Values=ec2-type^" --query ^"Reservations[].Instances[].[Tags[].Value, InstanceId, PublicIpAddress, State.Name]^" --profile AWS-profile & ^
+timeout /t 10 & ^
+aws ec2 stop-instances --instance-ids instance-id-1 instance-id-2 instance-id-3 instance-id-4 --profile AWS-profile & ^
+timeout /t 40"
+
+start cmd /k "echo EC2 machine new status && aws ec2 describe-instances --filters ^"Name=instance-type,Values=ec2-type^" --query ^"Reservations[].Instances[].[Tags[].Value, InstanceId, PublicIpAddress, State.Name]^" --profile AWS-profile"
